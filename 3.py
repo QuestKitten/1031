9125e3efec7de44611e69828176d38d60cc2c3a2
Quest <148282963+QuestKitten@users.noreply.github.com>
2023-11-01 14:34:19 +0900

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,10 +1,15 @@
 # Simple Calculator
+print("Welcome to simple calculator.")
+print("I will add/subtract/multiple/divide any two numbers you provide.")
+first_input = input("Enter in first number: ")
+second_input = input("Enter in second number: ")
+operation = input("Would you like to add/subtract/multiple/divide: ")
 
 # explicitly convert string type to float type
- # first_number = int(first_input)
- # second_number = int(second_input)
+first_number = int(first_input)
+second_number = int(second_input)
 
-# mathmatical symbols can also be used instead of the full word
+# mathmaticalsymbols can also be used instead of the full word
 if operation == "add":
     result = first_number + second_number
     print(f"Sum: {result}")
